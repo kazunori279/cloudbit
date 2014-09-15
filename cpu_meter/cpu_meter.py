759c8bc4481d845d10c6ddd61efe8f8084a91b7b
Kazunori Sato <kazunori279@gmail.com>
2014-09-15 14:39:00 +0000

    Fixed cpu_meter.py

--- cpu_meter/cpu_meter.py
+++ cpu_meter/cpu_meter.py
@@ -17,10 +17,12 @@ CBIT_DEVICE_ID='<<PUT YOUR DEVICE ID HERE>>'
 PATTERN_DSTAT_CPU_IDLE = re.compile(r'[^,]+,[^,]+,([\d\.]+),.*')
 
 def exec_dstat():
-    p = subprocess.Popen('dstat --output dstat.csv', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen('dstat --output dstat.csv', shell=True, stdout=subprocess.PIPE, \
+        stderr=subprocess.STDOUT)
 
 def read_dstat():
-    p = subprocess.Popen('tail -n 1 -f dstat.csv', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen('tail -n 1 -f dstat.csv', shell=True, stdout=subprocess.PIPE, \
+        stderr=subprocess.STDOUT)
     while True:
         yield p.stdout.readline().strip()
 
@@ -32,7 +34,9 @@ def extract_cpu_usage(line):
         return None 
 
 def post_cbit(value):
-    cmd = 'curl -i -X POST -H "Authorization: Bearer ' + CBIT_ACCESS_TOKEN + '" https://api-http.littlebitscloud.cc/devices/' + CBIT_DEVICE_ID + '/output -d percent=' + value + ' -d duration_ms=10000' 
+    cmd = 'curl -i -X POST -H "Authorization: Bearer ' + CBIT_ACCESS_TOKEN + \
+          '" https://api-http.littlebitscloud.cc/devices/' + CBIT_DEVICE_ID + \
+          '/output -d percent=' + value + ' -d duration_ms=10000' 
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     print 'Executing: ' + cmd
 
